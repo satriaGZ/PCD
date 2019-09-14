eaf342d26520c83dfa362eae66f84c25499f3206
satriaGZ <54826765+satriaGZ@users.noreply.github.com>
2019-09-14 10:31:02 +0800

    Code program di update
    Code program di update untuk proses translasi citra berwarna

--- translasiXY.m
+++ translasiXY.m
@@ -1,17 +1,17 @@
-a = imread('logo.jpg'); %membaca data gambar logo.jpg
-b = rgb2gray(a);  %mengubah citra berwarna menjadi citra keabuan
-[m n] = size(b);   %membaca dimensi dari citra b dimana m adalah jumlah baris dan n adalah jumlah kolom
-c = zeros(m,n);   % membuat matrik nol atau berisi nilai 0 dengan dimensi m x n atau m baris dan n kolom
+a = imread('unm.jpg'); %membaca data gambar logo.jpg
+b = a;  
+[m n o] = size(b);   %membaca dimensi dari citra b dimana m adalah jumlah baris dan n adalah jumlah kolom
+c = zeros(m,n,o);   % membuat matrik nol atau berisi nilai 0 dengan dimensi m x n x o atau m baris dan n kolom
 c = uint8(c);       %konversi type double menjadi type uint8 (type data citra)
-p = 400;            %nilai  translasi arah baris
-q = 0;              % nilai translasi arah kolom
+p = 10;            %nilai  translasi arah baris
+q = 100;              % nilai translasi arah kolom
 for K = 1:m
     for L = 1:n
         x = K+p;    %proses translasi arah baris x2 = x1 + p
         y = L+q;    %proses translasi arah kolom y2 = y1 + q dimana q adalah nilai pergeserannya
-        c(x,y) = b(K,L); %proses pemberian koordinat baru untuk seluruh intensitas citra
+        c(x,y,:) = b(K,L,:); %proses pemberian koordinat baru untuk seluruh intensitas citra
     end
 end
-d = c(1:m,1:n);    %menyesuaikan dimensi citranya
+d = c(1:m,1:n,:);    %menyesuaikan dimensi citranya
 figure(1);
 imshow(d)
